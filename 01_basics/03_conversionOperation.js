5521215bdfffe7e8c498845125cb9370901e067c
vrushabh3639 <155346369+vrushabh3639@users.noreply.github.com>
2025-06-06 04:50:11 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "vrushabh"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "30" => 30
+// "30abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "vrushabh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vrushabh" => true
+
+let someNumber = 30
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
